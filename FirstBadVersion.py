ff7d224626807363d94640b11d4ddbbd37f8871e
syedmujahedalih <syedmujahedalih@gmail.com>
2020-05-03 23:56:40 -0700

    Added code for Day 3 question

--- FirstBadVersion.py
+++ FirstBadVersion.py
@@ -1,5 +1,4 @@
-
-
+#isBadVersion API is already defined in LeetCode environment
 
 def firstBadVersion(self, n):
     start = 1
---
+++ RansomNote.py
@@ -0,0 +1,15 @@
+def canConstruct(ransomNote: str, magazine: str):
+    
+    if len(ransomNote) > len(magazine):
+        return False
+                
+    c1 = Counter(ransomNote)
+    c2 = Counter(magazine)
+    
+    for char, count in c1.items():
+        if char not in c2:
+            return False
+        if c2[char] < count:
+            return False
+    
+    return True
